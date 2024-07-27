56452332bccc9f32a35ebf0c52ceae43732be63a
alexmortelsison <alexmortelsison@gmail.com>
2024-07-27 21:23:36 +0900

    updated main

--- TypeConversion.java
+++ TypeConversion.java
@@ -6,11 +6,12 @@ public class TypeConversion {
         int purchasedBlocksOfCheese = 1;
 
         System.out.println("There are " + ants + " ants, and " + purchasedBlocksOfCheese + " block of cheese.");
-        System.out.println("Each ant carried " + (purchasedBlocksOfCheese / ants) + " from the total cheese amount"); 
+        System.out.println("Each ant carried " + ((double)purchasedBlocksOfCheese / ants) + " from the total cheese amount"); 
 
         double salary = 5423.94;
+        int roundedSalary = (int)salary;
         System.out.println("Mortgage Broker: What is your monthly salary in dollars?");
-        System.out.println("My monthly salary is: " + salary);
+        System.out.println("My monthly salary is: " + roundedSalary);
 
 
     }
